4e4f3f17ca12a3f98d740a63f149ac31dd4adad9
ymca <admin@example.com>
2023-11-11 15:58:51 +0800

    Sat Nov 11 15:58:50 CST 2023 M  blog/db/migrate/20231110141732_create_posts.rb M  blog/db/schema.rb M  blog/setup_oracle.sql

--- blog/setup_oracle.sql
+++ blog/setup_oracle.sql
@@ -3,3 +3,16 @@ DROP TABLESPACE indx_oracledb INCLUDING CONTENTS AND DATAFILES;
 CREATE USER oracleuser IDENTIFIED BY oraclepassword DEFAULT TABLESPACE indx_oracledb;
 GRANT connect,dba TO oracleuser;
 
+
+SELECT * FROM all_users;
+SELECT * FROM dba_users;
+SELECT * FROM user_users;
+
+select table_name from dba_tables;
+select table_name   from all_tables;
+select table_name   from user_tables;
+
+SELECT DISTINCT sgm.TABLESPACE_NAME , dtf.FILE_NAME
+FROM DBA_SEGMENTS sgm
+JOIN DBA_DATA_FILES dtf ON (sgm.TABLESPACE_NAME = dtf.TABLESPACE_NAME)
+WHERE sgm.OWNER = 'SCOTT'
